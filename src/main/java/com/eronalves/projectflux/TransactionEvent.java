789306ccbd410439874fa94bd070ac743f76628f
Eron Alves <eron.alves@rocketmail.com>
2026-04-01 15:29:51 -0300

    refactor: use better types in transaction event

--- src/main/java/com/eronalves/projectflux/TransactionEvent.java
+++ src/main/java/com/eronalves/projectflux/TransactionEvent.java
@@ -2,7 +2,8 @@ package com.eronalves.projectflux;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.UUID;
 
-public record TransactionEvent(String id, Instant timestamp, BigDecimal amount, String currency,
-    String userId) {
+public record TransactionEvent(UUID id, Instant timestamp, BigDecimal amount, String currency,
+        UUID userId) {
 }
