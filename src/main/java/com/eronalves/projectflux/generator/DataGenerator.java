a508c93cd5482f5e9228b038e3e0d441d6df0f19
Eron Alves <eron.alves@rocketmail.com>
2026-04-01 15:30:01 -0300

    feat: implement a data generator

--- src/main/java/com/eronalves/projectflux/generator/DataGenerator.java
+++ src/main/java/com/eronalves/projectflux/generator/DataGenerator.java
@@ -1,6 +1,12 @@
 package com.eronalves.projectflux.generator;
 
+import com.eronalves.projectflux.TransactionEvent;
+
 public interface DataGenerator<T> {
+
+  static DataGenerator<TransactionEvent> randomTransactionEventGenerator() {
+    return new RandomTransactionGenerator();
+  }
 
   T generate();
 
