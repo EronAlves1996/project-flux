2a61b58f823220ea9be7962e3edb48f52fac9a81
Eron Alves <eron.alves@rocketmail.com>
2026-04-06 13:30:36 -0300

    feat: implement ingestion logic

--- src/main/java/com/eronalves/projectflux/ingestion/IngestionService.java
+++ src/main/java/com/eronalves/projectflux/ingestion/IngestionService.java
@@ -1,6 +1,19 @@
 package com.eronalves.projectflux.ingestion;
 
+import java.util.List;
+import java.util.stream.Stream;
+import com.eronalves.projectflux.TransactionEvent;
+import com.eronalves.projectflux.generator.DataGenerator;
+
 public class IngestionService {
 
+  private DataGenerator<TransactionEvent> generator;
 
+  public IngestionService(DataGenerator<TransactionEvent> generator) {
+    this.generator = generator;
+  }
+
+  public List<TransactionEvent> ingestBatch(int batchSize) {
+    return Stream.generate(() -> generator.generate()).limit(batchSize).toList();
+  }
 }
