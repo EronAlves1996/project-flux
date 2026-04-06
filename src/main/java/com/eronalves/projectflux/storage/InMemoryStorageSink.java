0d6cf591d141d8ab7852cb543efe0fca63f0b7f5
Eron Alves <eron.alves@rocketmail.com>
2026-04-06 15:27:01 -0300

    feat: create and implement in memory storage sink

---
+++ src/main/java/com/eronalves/projectflux/storage/InMemoryStorageSink.java
@@ -0,0 +1,20 @@
+package com.eronalves.projectflux.storage;
+
+import java.util.LinkedList;
+import java.util.List;
+import com.eronalves.projectflux.TransactionEvent;
+
+class InMemoryStorageSink implements StorageSink<TransactionEvent> {
+
+  private final List<List<TransactionEvent>> storage;
+
+  public InMemoryStorageSink() {
+    storage = new LinkedList<>();
+  }
+
+  @Override
+  public void store(List<TransactionEvent> items) {
+    storage.add(items);
+  }
+
+}
