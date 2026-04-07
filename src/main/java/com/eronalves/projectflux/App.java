6c7caf901b93162357029ba845e79dbdc7c83a79
Eron Alves <eron.alves@rocketmail.com>
2026-04-07 15:47:35 -0300

    feat: apply categorization logic

--- src/main/java/com/eronalves/projectflux/App.java
+++ src/main/java/com/eronalves/projectflux/App.java
@@ -3,6 +3,7 @@ package com.eronalves.projectflux;
 import com.eronalves.projectflux.generator.DataGenerator;
 import com.eronalves.projectflux.ingestion.IngestionService;
 import com.eronalves.projectflux.storage.StorageSink;
+import com.eronalves.projectflux.transformers.TransactionCategorizer;
 
 /**
  * Hello world!
@@ -12,6 +13,9 @@ public class App {
 
     public static void main(String[] args) {
         new IngestionService(DataGenerator.randomTransactionEventGenerator(),
-                StorageSink.inMemory()).ingestBatch(BATCH_SIZE);
+                StorageSink.inMemory()).ingestBatch(BATCH_SIZE).forEach(transaction -> {
+                    var category = TransactionCategorizer.categorize(transaction.amount());
+                    IO.println("[" + category + "]" + transaction);
+                });
     }
 }
