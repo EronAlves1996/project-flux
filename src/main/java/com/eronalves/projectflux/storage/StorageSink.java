3797c09a975087025779575fff9ed39bd03edd1d
Eron Alves <eron.alves@rocketmail.com>
2026-04-08 15:11:30 -0300

    refactor: make possible create any type of in-memory storage sink

--- src/main/java/com/eronalves/projectflux/storage/StorageSink.java
+++ src/main/java/com/eronalves/projectflux/storage/StorageSink.java
@@ -1,12 +1,16 @@
 package com.eronalves.projectflux.storage;
 
 import java.util.List;
-import com.eronalves.projectflux.TransactionEvent;
+import com.eronalves.projectflux.model.TransactionEvent;
 
 public interface StorageSink<T> {
 
+  static <T> StorageSink<T> inMemoryGenericSink() {
+    return new InMemoryStorageSink<>();
+  }
+
   static StorageSink<TransactionEvent> inMemory() {
-    return new InMemoryStorageSink();
+    return inMemoryGenericSink();
   }
 
   void store(List<T> items);
