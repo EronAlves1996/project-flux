61d0b7b8cb9d0dfa01344dc242848c54817f24b7
Eron Alves <eron.alves@rocketmail.com>
2026-04-09 16:34:47 -0300

    feat: create method get total amount by category

--- src/main/java/com/eronalves/projectflux/serving/AnalyticsService.java
+++ src/main/java/com/eronalves/projectflux/serving/AnalyticsService.java
@@ -1,6 +1,22 @@
 package com.eronalves.projectflux.serving;
 
+import java.math.BigDecimal;
+import java.util.Map;
+import com.eronalves.projectflux.model.EnrichedTransactionEvent;
+import com.eronalves.projectflux.model.TransactionCategory;
+import com.eronalves.projectflux.storage.StorageSink;
+
 public class AnalyticsService {
+
+  private final StorageSink<EnrichedTransactionEvent> silverSink;
+
+  public AnalyticsService(StorageSink<EnrichedTransactionEvent> silverSink) {
+    this.silverSink = silverSink;
+  }
+
+  public Map<TransactionCategory, BigDecimal> getTotalAmountByCategory() {
+
+  }
 
 
 }
