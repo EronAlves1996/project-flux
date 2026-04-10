1f5e2f91dc82b898d644039bf470b41304758376
Eron Alves <eron.alves@rocketmail.com>
2026-04-10 17:29:16 -0300

    feat: implement orchestrator logic

--- src/main/java/com/eronalves/projectflux/orchestrator/PipelineOrchestrator.java
+++ src/main/java/com/eronalves/projectflux/orchestrator/PipelineOrchestrator.java
@@ -1,9 +1,46 @@
 package com.eronalves.projectflux.orchestrator;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import com.eronalves.projectflux.generator.DataGenerator;
+import com.eronalves.projectflux.ingestion.IngestionService;
+import com.eronalves.projectflux.model.EnrichedTransactionEvent;
+import com.eronalves.projectflux.model.TransactionEvent;
+import com.eronalves.projectflux.serving.AnalyticsService;
+import com.eronalves.projectflux.storage.StorageSink;
+import com.eronalves.projectflux.transformers.TransformationService;
+
 public class PipelineOrchestrator {
 
+  private final IngestionService ingestionService;
+  private final TransformationService transformationService;
+  private final AnalyticsService analyticsService;
+
+  public PipelineOrchestrator(AnalyticsService analyticsService,
+      TransformationService transformationService, IngestionService ingestionService) {
+    this.analyticsService = analyticsService;
+    this.transformationService = transformationService;
+    this.ingestionService = ingestionService;
+  }
+
   public PipelineRun execute(int batchSize) {
-    return null;
+    Instant startTime = Instant.now();
+
+    ingestionService.ingestBatch(batchSize);
+
+    int recordsProcessed = 0;
+    for (List<TransactionEvent> batch : ingestionService.getStorage().getAllBatches()) {
+      transformationService.transformAndStore(batch);
+      recordsProcessed += batch.size();
+    }
+
+    analyticsService.getTotalAmountByCategory().entrySet().stream().forEach(IO::println);
+
+    Instant endTime = Instant.now();
+
+    return new PipelineRun(UUID.randomUUID(), startTime, endTime, recordsProcessed,
+        PipelineStatus.SUCESS);
   }
 
 }
