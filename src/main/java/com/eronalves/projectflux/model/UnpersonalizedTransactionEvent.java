870bf5a3138d71e609ff613ca48409a05b5eab04
Eron Alves <eron.alves@rocketmail.com>
2026-04-23 15:45:50 -0300

    feat: add field schema version

--- src/main/java/com/eronalves/projectflux/model/UnpersonalizedTransactionEvent.java
+++ src/main/java/com/eronalves/projectflux/model/UnpersonalizedTransactionEvent.java
@@ -5,5 +5,11 @@ import java.time.Instant;
 import java.util.UUID;
 
 public record UnpersonalizedTransactionEvent(UUID id, Instant timestamp, BigDecimal amount,
-    String currency) {
+        String currency) {
+
+    private static final int SCHEMA_VERSION = 1;
+
+    public int schemaVersion() {
+        return SCHEMA_VERSION;
+    }
 }
