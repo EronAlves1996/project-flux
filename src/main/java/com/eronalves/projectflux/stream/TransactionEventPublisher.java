c3e86a919965acb3f8f26970f6ccaac2865fca4c
Eron Alves <eron.alves@rocketmail.com>
2026-04-24 15:15:47 -0300

    feat: create subscriber

--- src/main/java/com/eronalves/projectflux/stream/TransactionEventPublisher.java
+++ src/main/java/com/eronalves/projectflux/stream/TransactionEventPublisher.java
@@ -6,10 +6,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.Flow.Subscriber;
 import com.eronalves.projectflux.generator.DataGenerator;
-import com.eronalves.projectflux.model.TransactionEvent;
 import com.eronalves.projectflux.model.TransactionEventV1;
 
-public class TransactionEventPublisher implements Publisher<TransactionEvent>, AutoCloseable {
+public class TransactionEventPublisher implements Publisher<TransactionEventV1>, AutoCloseable {
 
   private final ScheduledExecutorService executor;
   private final DataGenerator<TransactionEventV1> generator;
@@ -25,7 +24,7 @@ public class TransactionEventPublisher implements Publisher<TransactionEvent>, A
   }
 
   @Override
-  public void subscribe(Subscriber<? super TransactionEvent> subscriber) {
+  public void subscribe(Subscriber<? super TransactionEventV1> subscriber) {
     executor.scheduleAtFixedRate(() -> subscriber.onNext(generator.generate()), 0, 100,
         TimeUnit.MILLISECONDS);
   }
---
+++ src/main/java/com/eronalves/projectflux/stream/TransactionEventSubscriber.java
@@ -0,0 +1,42 @@
+package com.eronalves.projectflux.stream;
+
+import java.util.List;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+import com.eronalves.projectflux.logging.PipelineLogger;
+import com.eronalves.projectflux.model.EnrichedTransactionEvent;
+import com.eronalves.projectflux.model.TransactionEventV1;
+import com.eronalves.projectflux.storage.StorageSink;
+import com.eronalves.projectflux.transformers.TransactionCategorizer;
+
+public class TransactionEventSubscriber implements Subscriber<TransactionEventV1> {
+
+  private final StorageSink<EnrichedTransactionEvent> storage;
+
+  public TransactionEventSubscriber(StorageSink<EnrichedTransactionEvent> storage) {
+    this.storage = storage;
+  }
+
+  @Override
+  public void onSubscribe(Subscription subscription) {
+    PipelineLogger.info(null, "Subscribed!!");
+  }
+
+  @Override
+  public void onNext(TransactionEventV1 item) {
+    storage.store(List
+        .of(new EnrichedTransactionEvent(TransactionCategorizer.categorize(item.amount()), item)));
+  }
+
+  @Override
+  public void onError(Throwable throwable) {
+    PipelineLogger.error(null, "An error has occurred", throwable);
+  }
+
+  @Override
+  public void onComplete() {
+    PipelineLogger.info(null, "Completed!");
+  }
+
+
+}
