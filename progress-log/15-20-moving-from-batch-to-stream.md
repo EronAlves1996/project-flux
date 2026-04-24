8e71afbf627cae03ef485371f719ec56f4db3063
Eron Alves <eron.alves@rocketmail.com>
2026-04-24 15:37:31 -0300

    Squash merge feat/streaming-ingestion into main

---
+++ progress-log/15-20-moving-from-batch-to-stream.md
@@ -0,0 +1,3 @@
+Simulated, using a native reactive framework from java, streaming event producing.
+In production, this can be bounded and unbounded and proper backpressure management is very important to achieve the proper results, assuming that the data produced,
+from systems can vary from times.
--- src/main/java/com/eronalves/projectflux/App.java
+++ src/main/java/com/eronalves/projectflux/App.java
@@ -11,6 +11,8 @@ import com.eronalves.projectflux.model.TransactionEventV1;
 import com.eronalves.projectflux.orchestrator.PipelineOrchestrator;
 import com.eronalves.projectflux.serving.AnalyticsService;
 import com.eronalves.projectflux.storage.StorageSink;
+import com.eronalves.projectflux.stream.TransactionEventPublisher;
+import com.eronalves.projectflux.stream.TransactionEventSubscriber;
 import com.eronalves.projectflux.transformers.TransformationService;
 
 /**
@@ -21,6 +23,19 @@ public class App {
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(5);
 
     public static void main(String[] args) {
+        try (var publisher = new TransactionEventPublisher()) {
+            StorageSink<EnrichedTransactionEvent> storage = StorageSink.inMemoryGenericSink();
+            var subscriber = new TransactionEventSubscriber(storage);
+            publisher.subscribe(subscriber);
+            Thread.sleep(5000);
+            publisher.close();
+        } catch (Exception ex) {
+            IO.println("An error has ocurred!!");
+            IO.println(ex);
+        }
+    }
+
+    public static void runBatch() {
         PipelineConfig environment = PipelineConfig.loadEnvironment();
 
         StorageSink<TransactionEventV1> bronzeSink = StorageSink.inMemory();
