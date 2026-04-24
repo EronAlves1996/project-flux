74bb39c1fad891da3aceb4cea30f4ac98793841a
Eron Alves <eron.alves@rocketmail.com>
2026-04-24 15:09:56 -0300

    feat: create publisher

---
+++ src/main/java/com/eronalves/projectflux/stream/TransactionEventPublisher.java
@@ -0,0 +1,39 @@
+package com.eronalves.projectflux.stream;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+import com.eronalves.projectflux.generator.DataGenerator;
+import com.eronalves.projectflux.model.TransactionEvent;
+import com.eronalves.projectflux.model.TransactionEventV1;
+
+public class TransactionEventPublisher implements Publisher<TransactionEvent>, AutoCloseable {
+
+  private final ScheduledExecutorService executor;
+  private final DataGenerator<TransactionEventV1> generator;
+
+  public TransactionEventPublisher() {
+    this(Executors.newScheduledThreadPool(1), DataGenerator.randomTransactionEventGenerator());
+  }
+
+  public TransactionEventPublisher(ScheduledExecutorService executor,
+      DataGenerator<TransactionEventV1> generator) {
+    this.executor = executor;
+    this.generator = generator;
+  }
+
+  @Override
+  public void subscribe(Subscriber<? super TransactionEvent> subscriber) {
+    executor.scheduleAtFixedRate(() -> subscriber.onNext(generator.generate()), 0, 100,
+        TimeUnit.MILLISECONDS);
+  }
+
+  @Override
+  public void close() throws Exception {
+    executor.shutdown();
+  }
+
+
+}
