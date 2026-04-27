f02918bd90ccc4a450f5688c2a959933b49b219c
Eron Alves <eron.alves@rocketmail.com>
2026-04-27 14:35:47 -0300

    feat: create retry utils

---
+++ src/main/java/com/eronalves/projectflux/resilience/RetryUtils.java
@@ -0,0 +1,23 @@
+package com.eronalves.projectflux.resilience;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+import com.eronalves.projectflux.storage.TransientStorageException;
+
+public class RetryUtils {
+
+  public static <T> T withRetry(Supplier<T> operation, int maxAttempts, Duration initialBackoff) {
+    for (int i = 0; i < maxAttempts; i++) {
+      try {
+        return operation.get();
+      } catch (TransientStorageException ex) {
+        try {
+          Thread.sleep(initialBackoff);
+        } catch (InterruptedException e) {
+          throw new FailedOperationException("Failed to retry", e);
+        }
+      }
+    }
+    throw new FailedOperationException("Failed operation");
+  }
+}
